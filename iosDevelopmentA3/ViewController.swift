8a839f9f3a6c52077c6fe56ce0f0b78c0e8c21c4
Jemima Khoo <jemima.khoo98@gmail.com>
2023-05-02 17:20:30 +1000

    setting up the repo

---
+++ iosDevelopmentA3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosDevelopmentA3
+//
+//  Created by Jemima on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
