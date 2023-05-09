2fabffdcfe5c39397d1ed8db37ecdfd093d108fd
Jemima Khoo <jemima.khoo98@gmail.com>
2023-05-09 18:08:31 +1000

    Set up storyboard for recipe and view controllers for recipes

---
+++ iosDevelopmentA3/Controller/ViewRecipeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewRecipeViewController.swift
+//  iosDevelopmentA3
+//
+//  Created by Jemima on 9/5/23.
+//
+
+import UIKit
+
+class ViewRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
