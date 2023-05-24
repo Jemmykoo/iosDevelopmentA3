e2fb42b76c8bb37849691163258964ef9a2afacf
Neme Kostanti Samour <79495238+ShadiSamour@users.noreply.github.com>
2023-05-24 17:17:04 +1000

    removed character limitation
    instead I made it so cells can have multiple lines.

--- iosDevelopmentA3/Controller/NewIngredientController.swift
+++ iosDevelopmentA3/Controller/NewIngredientController.swift
@@ -22,7 +22,6 @@ class NewIngredientController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ingredientNameTextField.delegate = self
         
     }
 
@@ -78,15 +77,8 @@ class NewIngredientController: UIViewController {
     }
 }
 
-extension NewIngredientController: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let currentString = (textField.text ?? "") as NSString
-        let replacementString = currentString.replacingCharacters(in: range, with: string)
 
-        return replacementString.count <= 26
-    }
-}
+
 
 
 
