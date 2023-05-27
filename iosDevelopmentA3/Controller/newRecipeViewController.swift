7174e60fe8721e614ef86eb8d9f06abfa9185b32
Jemima Khoo <jemima.khoo98@gmail.com>
2023-05-27 22:22:06 +1000

    WIP new recipe page

---
+++ iosDevelopmentA3/Controller/newRecipeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  newRecipeViewController.swift
+//  iosDevelopmentA3
+//
+//  Created by Jemima on 27/5/2023.
+//
+
+import UIKit
+
+class newRecipeViewController: UIViewController {
+
+    @IBOutlet weak var newRecipeNameField: UITextField!
+    @IBOutlet weak var newRecipeSearchBar: UISearchBar!
+    @IBOutlet weak var newRceipeMultiSelect: UITableView!
+    @IBOutlet weak var newRecipeStepsField: UITextField!
+    @IBOutlet weak var newRecipeSaveButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
